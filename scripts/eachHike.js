90f2f1a312404641e81255dfff5a27d78e3742b2
Tyrone Cheang <tcheang@my.bcit.ca>
2024-10-29 12:34:23 -0700

    added hike selection and firestore

---
+++ scripts/eachHike.js
@@ -0,0 +1,21 @@
+function displayHikeInfo() {
+    let params = new URL( window.location.href ); //get URL of search bar
+    let ID = params.searchParams.get( "docID" ); //get value for key "id"
+    console.log( ID );
+
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection( "hikes" )
+        .doc( ID )
+        .get()
+        .then( doc => {
+            thisHike = doc.data();
+            hikeCode = thisHike.code;
+            hikeName = doc.data().name;
+            
+            // only populate title, and image
+            document.getElementById( "hikeName" ).innerHTML = hikeName;
+            let imgEvent = document.querySelector( ".hike-img" );
+            imgEvent.src = "C:/Users/tchea/Downloads/School/comp1800/myhike/myhike_cheang_tyrone/images/" + hikeCode + ".jpg";
+        } );
+}
+displayHikeInfo();
